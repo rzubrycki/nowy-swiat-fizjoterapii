528229c2826b0bf6cc0a7e246e77d806a129aae4
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-11 14:21:01 +0100

    netfliy forms#5

--- src/sections/Contact/contact.js
+++ src/sections/Contact/contact.js
@@ -4,12 +4,44 @@ import Layout from '../../components/Layout/layout'
 export default () => (
   <Layout>
     <h2 className="sectionHeading">Kontakt</h2>
-    <form name="contact" data-netlify="true">
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <textarea name="message" />
-      <div data-netlify-recaptcha="true"></div>
-      <button type="submit">Send</button>
+    <form
+      name="kontakt"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+      <input type="hidden" name="form-name" value="kontakt" />
+      <p hidden>
+        <label>
+          Don’t fill this out:{' '}
+          <input name="bot-field" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your name:
+          <br />
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your email:
+          <br />
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <br />
+          <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   </Layout>
 )
