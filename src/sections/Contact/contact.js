23d8d91478a001dd19855b68d1cf502bd74d7e20
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-11 14:14:00 +0100

    netlify form#4

--- src/sections/Contact/contact.js
+++ src/sections/Contact/contact.js
@@ -4,16 +4,12 @@ import Layout from '../../components/Layout/layout'
 export default () => (
   <Layout>
     <h2 className="sectionHeading">Kontakt</h2>
-    <form
-      name="contact-form"
-      method="post"
-      data-netlify="true"
-      netlify-honeypot="bot-field"
-    >
-      <input hidden name="bot-field" />
-      <input type="text" placeholder="name" name="name" />
-      <div data-netlify-recaptcha />
-      <button>Send</button>
+    <form name="contact" data-netlify="true">
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message" />
+      <div data-netlify-recaptcha="true"></div>
+      <button type="submit">Send</button>
     </form>
   </Layout>
 )
