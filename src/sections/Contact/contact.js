80e61a06634e12479eb1e43097bb03f0961b4d62
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-11 13:51:46 +0100

    netlify form

--- src/sections/Contact/contact.js
+++ src/sections/Contact/contact.js
@@ -4,10 +4,14 @@ import Layout from '../../components/Layout/layout'
 export default () => (
   <Layout>
     <h2 className="sectionHeading">Kontakt</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores placeat
-      illum saepe laborum? Illum praesentium, alias vel non dolore hic odio
-      error explicabo eos nam officia repellendus odit debitis sit.
-    </p>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="text" name="name" placeholder="your name" />
+      <button>send</button>
+    </form>
   </Layout>
 )
