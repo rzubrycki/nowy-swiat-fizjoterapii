60f220eda3a83d069ce0c3242917d0540fee0177
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-12 22:11:25 +0100

    Class bug#3

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -9,53 +9,52 @@ export default class extends Component {
 
   render() {
     return (
-      <div className={styles.formWrapper}>
-        <form
-          name="kontakt"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <h2 className="sectionHeading">Kontakt</h2>
-          <input type="hidden" name="form-name" value="kontakt" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
-          </p>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="Imię"
-            className={styles.formControl}
-          />
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Nazwisko"
-            className={styles.formControl}
-          />
-          <input
-            type="text"
-            name="phone"
-            placeholder="Telefon"
-            className={styles.formControl}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Adres email"
-            className={styles.formControl}
-          />
-          <textarea
-            name="message"
-            placeholder="Treść wiadomości"
-            rows="4"
-            className={styles.formControl}
-          />
-          <button type="submit">Wyślij</button>
-        </form>
-      </div>
+      <form
+        name="kontakt"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        className={styles.formWrapper}
+      >
+        <h2 className="sectionHeading">Kontakt</h2>
+        <input type="hidden" name="form-name" value="kontakt" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Imię"
+          className={styles.formControl}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Nazwisko"
+          className={styles.formControl}
+        />
+        <input
+          type="text"
+          name="phone"
+          placeholder="Telefon"
+          className={styles.formControl}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Adres email"
+          className={styles.formControl}
+        />
+        <textarea
+          name="message"
+          placeholder="Treść wiadomości"
+          rows="4"
+          className={styles.formControl}
+        />
+        <button type="submit">Wyślij</button>
+      </form>
     )
   }
 }
