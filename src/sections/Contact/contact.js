949b7d9bb5245daf996f89a47b3859e8bafceb25
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-12 20:27:12 +0100

    Class bug

--- src/sections/Contact/contact.js
+++ src/sections/Contact/contact.js
@@ -3,13 +3,13 @@ import React from 'react'
 import styles from './contact.module.scss'
 import Layout from '../../components/Layout/layout'
 import Map from '../../components/Map/map'
-import ContactForm from '../../components/ContactForm/contactForm'
+// import ContactForm from '../../components/ContactForm/contactForm'
 
 export default () => (
   <Layout>
     <div className={styles.contactWrapper}>
       <Map />
-      <ContactForm />
+      {/* <ContactForm /> */}
     </div>
   </Layout>
 )
