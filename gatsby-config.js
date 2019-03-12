58f295e7729ce151d8f63b57898b659d98d94f0d
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-12 15:30:48 +0100

    Custom marker, form styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,5 +58,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-leaflet`,
   ],
 }
--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -3,15 +3,11 @@ import React, { Component } from 'react'
 import Address from '../Sidebar/Address/address'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
 
-delete L.Icon.Default.prototype._getIconUrl
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-})
+let leaflet;
+if (typeof window !== 'undefined') {
+  leaflet = require('leaflet');
+}
 
 export default class extends Component {
   constructor() {
@@ -30,18 +26,19 @@ export default class extends Component {
       }
     }
     const position = [this.state.lat, this.state.lng]
-    return (
-      <Map center={position} zoom={this.state.zoom} scrollWheelZoom={false}>
-        <TileLayer
-
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position} ref={initMarker}>
-          <Popup>
-            <Address />
-          </Popup>
-        </Marker>
-      </Map>
-    )
+    if (typeof window !== 'undefined') {
+      const customMarker = leaflet.icon({ iconUrl: require('../../assets/images/marker.svg'), })
+      return (
+        <Map center={position} zoom={this.state.zoom} scrollWheelZoom={false}>
+          <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          <Marker position={position} ref={initMarker} icon={customMarker}>
+            <Popup>
+              <Address />
+            </Popup>
+          </Marker>
+        </Map>
+      )
+    }
+    return null
   }
 }
