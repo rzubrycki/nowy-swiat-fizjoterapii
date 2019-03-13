fb0ece7b881c322eff3e1236f0242350a6db1514
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-13 12:04:34 +0100

    Handle form state

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -1,60 +1,100 @@
 import React, { Component } from 'react'
 
 import styles from './contactForm.module.scss'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+
+const ContactSchema = Yup.object().shape({
+  firstName: Yup.string().required('Pole jest wymagane.'),
+  lastName: Yup.string().required('Pole jest wymagane.'),
+  email: Yup.string()
+    .email('Niepoprawny adres email. Spróbuj ponownie.')
+    .required('Pole jest wymagane.'),
+  message: Yup.string()
+    .min(2, 'Wiadomość jest za krótka. Napisz coś więcej.')
+    .required('Pole jest wymagane.'),
+})
 
 export default class extends Component {
-  constructor() {
-    super()
-  }
-
   render() {
     return (
-      <form
-        name="kontakt"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        className={styles.formWrapper}
-      >
-        <h2 className="sectionHeading">Kontakt</h2>
-        <input type="hidden" name="form-name" value="kontakt" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" />
-          </label>
-        </p>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="Imię"
-          className={styles.formControl}
-        />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Nazwisko"
-          className={styles.formControl}
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Telefon"
-          className={styles.formControl}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Adres email"
-          className={styles.formControl}
-        />
-        <textarea
-          name="message"
-          placeholder="Treść wiadomości"
-          rows="4"
-          className={styles.formControl}
-        />
-        <button type="submit">Wyślij</button>
-      </form>
+      <div className={styles.formWrapper}>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: '',
+          }}
+          validationSchema={ContactSchema}
+          onSubmit={values => {
+            // same shape as initial values
+            console.log(values)
+          }}
+        >
+          {({ errors, touched, isValid }) => (
+            <Form
+              name="kontakt"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+            {console.log(isValid)}
+              <h2 className="sectionHeading">Kontakt</h2>
+              <input type="hidden" name="form-name" value="kontakt" />
+              <p hidden>
+                <label>
+                  Don’t fill this out: <input name="bot-field" />
+                </label>
+              </p>
+              <div className={styles.fieldWrapper}>
+                <Field
+                  name="firstName"
+                  className={styles.formControl}
+                  placeholder="Imię"
+                />
+                {errors.firstName && touched.firstName ? (
+                  <div className={styles.errorMessage}>{errors.firstName}</div>
+                ) : null}
+              </div>
+              <div className={styles.fieldWrapper}>
+                <Field
+                  name="lastName"
+                  className={styles.formControl}
+                  placeholder="Nazwisko"
+                />
+                {errors.lastName && touched.lastName ? (
+                  <div className={styles.errorMessage}>{errors.lastName}</div>
+                ) : null}
+              </div>
+              <div className={styles.fieldWrapper}>
+                <Field
+                  name="email"
+                  type="email"
+                  className={styles.formControl}
+                  placeholder="Email"
+                />
+                {errors.email && touched.email ? (
+                  <div className={styles.errorMessage}>{errors.email}</div>
+                ) : null}
+              </div>
+              <div className={styles.fieldWrapper}>
+                <Field
+                  component="textarea"
+                  name="message"
+                  placeholder="Treść wiadomości"
+                  rows="4"
+                  className={styles.formControl}
+                />
+                {errors.message && touched.message ? (
+                  <div className={styles.errorMessage}>{errors.message}</div>
+                ) : null}
+              </div>
+              <button type="submit" disabled={isValid === false}>Wyślij</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     )
   }
 }
