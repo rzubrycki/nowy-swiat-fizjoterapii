8159ce3de3bf009ca92117e8066b790dc98da20c
Robert Zubrycki <robert.zubrycki@intive.com>
2019-03-16 12:27:17 +0100

    Replace dummy content

--- src/sections/Offers/offersData.js
+++ src/sections/Offers/offersData.js
@@ -1,12 +1,14 @@
 export const IndividualWorkWithPatient = [
   {
     title: `Konsultacja / wizyta oceniająca`,
-    content: `Terapia manualna jest najbardziej bolesna`,
+    content: `Konsultacja to ocena stanu pacjenta, odbywająca się na pierwszym spotkaniu z terapeutą. Podczas tej wizyty przeprowadzany jest dokładny wywiad, oglądana jest postawa pacjenta i przeprowadzane są testy funkcjonalne. Wszystko to pozwala należycie zdiagnozować pacjenta i wytyczyć kierunek terapii. Po oględzinach, jeszcze podczas tej wizyty przeprowadzana jest także pierwsza wstępna terapia.`,
     duration: `ok. 1h`,
   },
   {
     title: `Terapia manualna`,
-    content: `It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.It doesn't matter how much content you put in each accordian. You only have to define one posed component that animates to "auto" and reuse that.`,
+    content: `Terapia manualna polega na ucisku odpowiednich punktów na ciele, w celu przywrócenia równowagi mięśniowej. Wiąże się z pracą rąk fizjoterapeuty na ciele pacjenta. Może być skierowana na tkanki miękkie lub stawy. Często przypomina masaż.
+
+    Terapia manualna ma zastosowanie przy bólach kręgosłupa, uszkodzeniach stawów i tkanek miękkich, złamaniach, zwichnięciach, wadach postawy, stanach napięcia mięśni, nerwobólach, a także w leczeniu pooperacyjnym czy po urazach sportowych.`,
     duration: `45min`,
   },
   {
@@ -34,27 +36,26 @@ export const IndividualWorkWithPatient = [
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius officia autem distinctio, dolore, sint possimus quae doloremque accusamus recusandae nesciunt repellendus repellat minus dolor fuga non. Iure architecto autem ut?`,
     duration: `50min`,
   },
-  {
-    title: `Kinezyterapia indywidualna`,
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius officia autem distinctio, dolore, sint possimus quae doloremque accusamus recusandae nesciunt repellendus repellat minus dolor fuga non. Iure architecto autem ut?`,
-    duration: `45min`,
-  },
 ]
 
 export const PhysioTherapy = [
   {
     title: `Fala uderzeniowa (RSW)`,
-    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit dolores sit quas eius magnam atque possimus ut, officia deleniti dolorum repellendus quidem est sunt libero tempore laboriosam aut ad expedita.`,
+    content: `Terapia przy użyciu Fali uderzeniowej jest nieinwazyjnym rozwiązaniem pozwalającym na leczenie przewlekłego bólu w układzie mięśniowo-szkieletowym. Fale uderzeniowe są falami akustycznymi przenoszącymi energię w bolące miejsce. Zabieg ten ma zastosowanie między innymi w: zespole bolesnego barku, łokciu tenisisty/golfisty, bólu ścięgna Achillesa, zapaleniach ścięgien, czy ostrodze piętowej.
+
+    Proces leczenia oraz regeneracji następuje dzięki poprawie metabolizmu i mikrocyrkulacji, zmniejszeniu napięcia mięśni i zahamowaniu skurczów, usuwaniu z chorej tkanki szkodliwych substancji (zwapniałe złogi, kwas mlekowy) oraz zwiększeniu produkcji kolagenu, który jest niezbędny do odbudowy uszkodzonych tkanek.
+
+    Fala uderzeniowa ma także zastosowanie w walce z cellulitem, dzięki wykorzystaniu specjalistycznej nakładki. Bezinwazyjne ujędrnianie skóry pozwala nie tylko na redukcję cellulitu, ale także na zmniejszenie widocznych blizn i rozstępów.`,
     duration: `*`,
   },
   {
     title: `TR THERAPY`,
-    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit dolores sit quas eius magnam atque possimus ut, officia deleniti dolorum repellendus quidem est sunt libero tempore laboriosam aut ad expedita.`,
+    content: `TR-Therapy to zabieg podczas, którego przez tkanki przepływa prąd powodując ich selektywne przegrzanie. Do dyspozycji są dwie różne elektrody (pojemnościowa i rezystywna) umożliwiające pracę na różnych głębokościach tkanek. Elektroda pojemnościowa skupia energię w mięśniach, zaś elektroda rezystywna działa na przyczepy mięśni oraz więzadła. Zabieg odczuwany jest w postaci delikatnego ciepła. Efekty terapeutyczne, jakie można uzyskać przy użyciu TR-Therapy to: rozluźnienie mięśni, przyspieszenie regeneracji tkanek (poprawa miejscowego metabolizmu, rozszerzenie naczyń krwionośnych), redukcja obrzęków (poprawa krążenia limfy), a w konsekwencji zmniejszenie objawów bólowych.`,
     duration: `30min`,
   },
   {
     title: `Elektroterapia`,
-    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit dolores sit quas eius magnam atque possimus ut, officia deleniti dolorum repellendus quidem est sunt libero tempore laboriosam aut ad expedita.`,
+    content: `Przepływ prądu przez ciało pacjenta jest bodźcem leczniczym. Dzięki temu można poprawić ukrwienie, zmniejszyć napięcie mięśniowe i bóle oraz przyspieszyć proces wchłaniania się obrzęków. Do dyspozycji mamy następujące rodzaje prądów: TENS, IF, Trabert, DD, galwaniczny, elektrostymulacja.`,
     duration: `15-20min`,
   },
   {
