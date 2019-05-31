0c4c7c356cb9a8f855e16c423685029f2e963446
Robert Zubrycki <robert.zubrycki@intive.com>
2019-05-31 12:00:14 +0200

    Fix error

--- src/pages/success.js
+++ src/pages/success.js
@@ -3,17 +3,10 @@ import React from 'react'
 import logo from '../assets/images/logo.svg'
 import { Link } from 'gatsby'
 
-export function Success() {
-  return (
-    <div className="successWrapper">
-      <img src={logo} alt="logo Nowy Świat Fizjoterapii" />
-      <p>
-        Dziękujemy za wysłanie formularza. <br />
-        Odezwiemy się najszybciej jak to możliwe.
-      </p>
-      <Link to="/">
-        <button>Powrót</button>
-      </Link>
-    </div>
-  )
-}
+export default () => (
+  <div className="successWrapper">
+    <img src={logo} alt="logo Nowy Świat Fizjoterapii" />
+    <p>Dziękujemy za wysłanie formularza. <br />Odezwiemy się najszybciej jak to możliwe.</p>
+    <Link to="/"> <button>Powrót</button></Link>
+  </div>
+)
